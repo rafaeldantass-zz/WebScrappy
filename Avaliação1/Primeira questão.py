24bac138c286e32bca01546cb3e00bd5682f5a15
rdantasss <39377186+rdantasss@users.noreply.github.com>
2020-01-10 20:59:09 -0300

    Add files via upload

---
+++ Avaliação1/Primeira questão.py
@@ -0,0 +1,16 @@
+import scrapy
+from scrapy.shell import inspect_response
+import re
+
+class UolSpider(scrapy.Spider):
+    name = "Uol"
+    start_urls = {
+        'https://www.uol.com.br'
+    }
+    def parse (self, response):
+        valor = response.css(".HU_currency__quote_up::text").extract_first()
+        if valor is None:
+            valor = response.css(".HU_currency__quote_down::text").extract_first()
+		print("A cotação atual do dólar é: R$ {}".format(valor))
+		
+		
