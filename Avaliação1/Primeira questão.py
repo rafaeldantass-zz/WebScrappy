52a5a248787cc971d0ed085e05fec46c7d033702
rdantasss <39377186+rdantasss@users.noreply.github.com>
2020-01-10 21:14:48 -0300

    Update Primeira questão.py

--- Avaliação1/Primeira questão.py
+++ Avaliação1/Primeira questão.py
@@ -2,15 +2,20 @@ import scrapy
 from scrapy.shell import inspect_response
 import re
 
+
+
 class UolSpider(scrapy.Spider):
     name = "Uol"
+
     start_urls = {
         'https://www.uol.com.br'
     }
+
     def parse (self, response):
-        valor = response.css(".HU_currency__quote_up::text").extract_first()
-        if valor is None:
-            valor = response.css(".HU_currency__quote_down::text").extract_first()
-		print("A cotação atual do dólar é: R$ {}".format(valor))
-		
+       	elementos = response.css(".HU_currency__item:nth-child(1)")
+
+        for elemento in elementos:
+            #    open_in_browser(response)
+                
+                 print('A COTAÇÃO DOLAR É ' + elemento.css("span::text")[1].extract())
 		
